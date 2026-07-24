35a4b75079eca9d83acaf13ad2a96415533fb8ab
code-rmendoza <roger.mendoza.m24@gmail.com>
2026-07-24 16:50:29 -0400

    fix: disable R8 resource shrinking to prevent APK package parsing failure

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
             } else {
                 signingConfigs.getByName("debug")
             }
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
